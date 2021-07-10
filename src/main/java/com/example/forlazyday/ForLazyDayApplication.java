092b2b50eca9ea13fa60e7fc7a8f21ff86c97790
TonyTor <Tony.Tor2016@yandex.ru>
2021-07-10 21:32:52 +0400

    Today I was too lazy to program. So I created this commit.

---
+++ src/main/java/com/example/forlazyday/ForLazyDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.forlazyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForLazyDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForLazyDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forlazyday/ForLazyDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forlazyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForLazyDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
